dd3c727020ad7ed5000ee45262f5dc2d35588285
Bharat Poptwani <bharat.poptwani@gmail.com>
2020-10-19 20:50:46 +0530

    added key prop in a child component

--- src/exercise/07.js
+++ src/exercise/07.js
@@ -37,7 +37,7 @@ function App() {
       <ul style={{listStyle: 'none', paddingLeft: 0}}>
         {items.map(item => (
           // 🐨 add a key prop to the <li> below. Set it to item.id
-          <li>
+          <li key={item.id}>
             <button onClick={() => removeItem(item)}>remove</button>{' '}
             <label htmlFor={`${item.value}-input`}>{item.value}</label>{' '}
             <input id={`${item.value}-input`} defaultValue={item.value} />
